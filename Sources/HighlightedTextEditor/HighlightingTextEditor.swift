b89c357fa068bf826a1ee47bd21572658dddbe06
kyle-n <kylebnazario@gmail.com>
2020-08-31 11:57:17 -0600

    Sets it to system font

--- Sources/HighlightedTextEditor/HighlightingTextEditor.swift
+++ Sources/HighlightedTextEditor/HighlightingTextEditor.swift
@@ -26,6 +26,14 @@ extension HighlightingTextEditor {
     static func getHighlightedText(text: String, highlightRules: [HighlightRule]) -> NSMutableAttributedString {
         let highlightedString = NSMutableAttributedString(string: text)
         let all = NSRange(location: 0, length: text.count)
+        
+        #if os(macOS)
+        let systemFont = NSFont.systemFont(ofSize: NSFont.systemFontSize)
+        #else
+        let systemFont = UIFont.systemFont(ofSize: NSFont.systemFontSize)
+        #endif
+        highlightedString.addAttribute(.font, value: systemFont, range: all)
+        
         highlightRules.forEach { rule in
             let matches = rule.pattern.matches(in: text, options: [], range: all)
             matches.forEach { match in
