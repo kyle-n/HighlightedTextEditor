6c8c3580d69ba7ead1106e4a391b9374149fe325
kyle-n <kylebnazario@gmail.com>
2020-09-08 15:56:36 -0600

    Consolidates system extensions

--- Sources/HighlightedTextEditor/System Extensions/TextFormattingRule.swift
+++ Sources/HighlightedTextEditor/System Extensions/TextFormattingRule.swift
@@ -20,3 +20,18 @@ extension TextFormattingRule {
     }
 }
 #endif
+
+#if os(macOS)
+extension TextFormattingRule {
+    @available(macOS 11.0, *)
+    public init(foregroundColor color: Color, fontTraits: SymbolicTraits = []) {
+        self.init(key: .foregroundColor, value: NSColor(color), fontTraits: fontTraits)
+    }
+    
+    @available(macOS 11.0, *)
+    public init(highlightColor color: Color, fontTraits: SymbolicTraits = []) {
+        self.init(key: .backgroundColor, value: NSColor(color), fontTraits: fontTraits)
+    }
+}
+#endif
+
