c5707e9cba36715b9b3bd1675834b1ac7877bad4
kyle-n <kylebnazario@gmail.com>
2020-09-08 08:18:47 -0600

    UIFont extension for easy trait addition

---
+++ Sources/HighlightedTextEditor/UIFont.swift
@@ -0,0 +1,38 @@
+// Maksymilian Wojakowski
+// https://stackoverflow.com/questions/4713236/how-do-i-set-bold-and-italic-on-uilabel-of-iphone-ipad
+
+import Foundation
+#if os(iOS)
+import UIKit
+#endif
+
+extension UIFont {
+    var bold: UIFont {
+        return with(.traitBold)
+    }
+
+    var italic: UIFont {
+        return with(.traitItalic)
+    }
+
+    var boldItalic: UIFont {
+        return with([.traitBold, .traitItalic])
+    }
+
+
+    func with(_ traits: UIFontDescriptor.SymbolicTraits...) -> UIFont {
+        let traitSet: UIFontDescriptor.SymbolicTraits = UIFontDescriptor.SymbolicTraits(traits).union(self.fontDescriptor.symbolicTraits);
+        guard let descriptor = self.fontDescriptor.withSymbolicTraits(traitSet) else {
+            return self
+        }
+        return UIFont(descriptor: descriptor, size: 0)
+    }
+
+    func without(_ traits: UIFontDescriptor.SymbolicTraits...) -> UIFont {
+        let traitSet = self.fontDescriptor.symbolicTraits.subtracting(UIFontDescriptor.SymbolicTraits(traits))
+        guard let descriptor = self.fontDescriptor.withSymbolicTraits(traitSet) else {
+            return self
+        }
+        return UIFont(descriptor: descriptor, size: 0)
+    }
+}
