716c943b8eff8e48243324de78a871e6f8e23a9c
Joannis Orlandos <joannis@orlandos.nl>
2020-10-26 15:38:56 +0100

    Improve the overall editing experience on iOS

--- Sources/HighlightedTextEditor/HighlightedTextEditor.iOS.swift
+++ Sources/HighlightedTextEditor/HighlightedTextEditor.iOS.swift
@@ -46,23 +46,36 @@ public struct HighlightedTextEditor: UIViewRepresentable, HighlightingTextEditor
         uiView.isScrollEnabled = false
         
         let cursor = uiView.selectedRange
-        defer {
-            if uiView.attributedText.length <= cursor.upperBound {
+        if cursor.upperBound <= uiView.attributedText.length {
+            context.coordinator.updating = cursor
+            // Attributedtext is updating the position the next tick
+            // Therefore, so are we
+            DispatchQueue.main.async {
                 uiView.selectedRange = cursor
+                context.coordinator.updating = nil
             }
         }
         
         let highlightedText = HighlightedTextEditor.getHighlightedText(text: text, highlightRules: highlightRules)
 
         uiView.attributedText = highlightedText
-        uiView.isScrollEnabled = true
+        DispatchQueue.main.async {
+            uiView.isScrollEnabled = true
+        }
     }
 
     public class Coordinator: NSObject, UITextViewDelegate {
         var parent: HighlightedTextEditor
+        var updating: NSRange?
 
         init(_ markdownEditorView: HighlightedTextEditor) {
             self.parent = markdownEditorView
+        }
+        
+        public func textViewDidChangeSelection(_ textView: UITextView) {
+            if let updating = updating {
+                textView.selectedRange = updating
+            }
         }
         
         public func textViewDidChange(_ textView: UITextView) {
