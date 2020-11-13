bf1d8c9986a6aa2f75f0487ba32f39a69960c2f1
kyle-n <kylebnazario@gmail.com>
2020-11-13 09:51:28 -0700

    Attributed strings with marked text
    - UIKit editor saves attributed strings with marked text, supporting languages with two-stage autocomplete
    - Tested on Chinese Pinyin and Japanese Kana keyboards on iPadOS 14.2

---
+++ Sources/HighlightedTextEditor/System Extensions/UITextView.swift
@@ -0,0 +1,20 @@
+//
+//  UITextView.swift
+//  
+//
+//  Created by Kyle Nazario on 11/13/20.
+//
+
+#if os(iOS)
+import Foundation
+import UIKit
+
+extension UITextView {
+    var markedTextNSRange: NSRange? {
+        guard let markedTextRange = markedTextRange else { return nil }
+        let location = offset(from: beginningOfDocument, to: markedTextRange.start)
+        let length = offset(from: markedTextRange.start, to: markedTextRange.end)
+        return NSRange(location: location, length: length)
+    }
+}
+#endif
