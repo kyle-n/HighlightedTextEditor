dc0700a30c03f240f74276e5913a2a5bd52f28c9
kyle-n <kylebnazario@gmail.com>
2020-11-25 12:03:50 -0700

    Sample project for tests

---
+++ Tests/Essayist/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Kyle Nazario on 11/25/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
