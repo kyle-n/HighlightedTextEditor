ee533083078765967e34fb0d2101f10cfb5ab0f2
kyle-n <kylebnazario@gmail.com>
2020-11-27 10:45:54 -0700

    E2E test to prevent jump-to-end while typing in the middle bug

--- Tests/Essayist/iOS-EssayistUITests/iOS_EssayistUITests.swift
+++ Tests/Essayist/iOS-EssayistUITests/iOS_EssayistUITests.swift
@@ -38,16 +38,54 @@ class iOS_EssayistUITests: XCTestCase {
         assertSnapshot(matching: AnyView(MarkdownEditorB()), as: device)
         assertSnapshot(matching: AnyView(MarkdownEditorC()), as: device)
     }
-    
+
     func testURLPresetHighlighting() {
         assertSnapshot(matching: AnyView(URLEditor()), as: device)
     }
-    
+
     func testCustomFontTraits() {
         assertSnapshot(matching: AnyView(FontTraitEditor()), as: device)
     }
-    
+
     func testCustomNSAttributedStringKeyValues() {
         assertSnapshot(matching: AnyView(NSAttributedStringKeyEditor()), as: device)
+    }
+    
+    func testTypingInMiddle() {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let hlteTextView = app.textViews["hlte"]
+        hlteTextView.tap()
+        
+        app.keys["A"].tap()
+        
+        let returnButton = app.buttons["Return"]
+        returnButton.tap()
+        returnButton.tap()
+        
+        app.keys["B"].tap()
+        returnButton.tap()
+        returnButton.tap()
+        
+        app.keys["D"].tap()
+        returnButton.tap()
+        returnButton.tap()
+        
+        // Move selection to after "B"
+        hlteTextView
+            .coordinate(withNormalizedOffset: CGVector(dx: 0, dy: 0))
+            .withOffset(CGVector(dx: 30, dy: 60))
+            .tap()
+        returnButton.tap()
+        returnButton.tap()
+        app.keys["C"].tap()
+        app.keys["a"].tap()
+        sleep(2)
+        app.keys["r"].tap()
+        sleep(4)
+        app.keys["s"].tap()
+        
+        XCTAssertEqual(hlteTextView.value as! String, "A\n\nB\n\nCars\n\nD")
     }
 }
