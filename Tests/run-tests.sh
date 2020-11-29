2d76040e5c4423ea997c302361bb3699992b5c6a
kyle-n <kylebnazario@gmail.com>
2020-11-29 12:18:00 -0700

    Splits AppKit and UIKit test scripts

--- Tests/run-tests.sh
+++ Tests/run-tests.sh
@@ -1,22 +1,8 @@
 #!/usr/bin/env bash
 
-test_project_path='./Essayist/Essayist.xcodeproj'
-test_project_name='Essayist'
-
 # Make script able to run from any directory
 script_dirname=$(dirname "$0")
 cd $script_dirname
 
-killall Simulator
-defaults write com.apple.iphonesimulator ConnectHardwareKeyboard -bool false
-open -a Simulator.app
-sleep 2
-osascript -e 'tell application "Simulator" to activate'
-osascript -e 'tell application "System Events" to keystroke "k" using {command down, shift down}'
-osascript -e 'tell application "System Events" to keystroke "k" using {command down, shift down}'
-xcodebuild -resolvePackageDependencies
-xcodebuild test -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 11' -scheme "$test_project_name (iOS)" -project $test_project_path || true
-killall Simulator
-defaults write com.apple.iphonesimulator ConnectHardwareKeyboard -bool true
-
-xcodebuild test -sdk macosx11.0 -destination 'platform=OS X,arch=x86_64' -scheme "$test_project_name (macOS)" -project $test_project_path
+./uikit-tests.sh
+./appkit-tests.sh
