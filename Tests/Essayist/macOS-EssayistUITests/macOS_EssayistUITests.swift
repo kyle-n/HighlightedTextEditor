bc8f36a81cc6910302358623deeef231106762ed
kyle-n <kylebnazario@gmail.com>
2020-11-29 10:36:46 -0700

    E2E test for typing in the middle of text

--- Tests/Essayist/macOS-EssayistUITests/macOS_EssayistUITests.swift
+++ Tests/Essayist/macOS-EssayistUITests/macOS_EssayistUITests.swift
@@ -15,48 +15,20 @@ class macOS_EssayistUITests: XCTestCase {
         continueAfterFailure = true
     }
     
-//    func testMarkdownPresetHighlighting() {
-//        assertSnapshot(matching: AnyView(MarkdownEditorA()), as: .image)
-//        assertSnapshot(matching: AnyView(MarkdownEditorB()), as: device)
-//        assertSnapshot(matching: AnyView(MarkdownEditorC()), as: device)
-//    }
-
-    func testURLPresetHighlighting() {
-        let urlEditor = URLEditor()
-        let contentRect = NSRect(x: 0, y: 0, width: 480, height: 480)
-
-        // Create the window and set the content view.
-        let window = NSWindow(
-            contentRect: contentRect,
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: urlEditor)
-        window.makeKeyAndOrderFront(nil)
-
-        let newWindow = NSWindow(
-            contentRect: contentRect,
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-
-        newWindow.contentView = NSHostingView(rootView: urlEditor)
-
-        let myNSBitMapRep = newWindow.contentView!.bitmapImageRepForCachingDisplay(in: contentRect)!
-
-        newWindow.contentView!.cacheDisplay(in: contentRect, to: myNSBitMapRep)
-
-        let myNSImage = NSImage(size: myNSBitMapRep.size)
-        myNSImage.addRepresentation(myNSBitMapRep)
+    func testTypingInMiddle() {
+        let app = XCUIApplication()
+        app.launch()
         
-        assertSnapshot(matching: myNSImage, as: .image)
+        let editor = app.textViews.firstMatch
+        editor.click()
+        editor.typeText("1 3")
+        
+        editor.typeKey(.leftArrow, modifierFlags: [])
+        editor.typeKey(.leftArrow, modifierFlags: [])
+        editor.typeKey(.space, modifierFlags: [])
+        editor.typeText("two")
+        
+        XCTAssertEqual(editor.value as! String, "1 two 3")
     }
-
-//    func testCustomFontTraits() {
-//        assertSnapshot(matching: AnyView(FontTraitEditor()), as: device)
-//    }
-//
-//    func testCustomNSAttributedStringKeyValues() {
-//        assertSnapshot(matching: AnyView(NSAttributedStringKeyEditor()), as: device)
-//    }
+    
 }
