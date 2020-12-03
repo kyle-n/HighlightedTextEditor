845a30591022708a32f0385feeebbe2ce322a583
tdt <hstdt@163.com>
2020-12-03 20:35:47 +0800

    Support multistage text input

--- Sources/HighlightedTextEditor/HighlightedTextEditor.iOS.swift
+++ Sources/HighlightedTextEditor/HighlightedTextEditor.iOS.swift
@@ -94,6 +94,13 @@ public struct HighlightedTextEditor: UIViewRepresentable, HighlightingTextEditor
         }
         
         public func textViewDidChange(_ textView: UITextView) {
+
+            // For Multistage Text Input(Chinese, Japanese)
+            guard  textView.markedTextRange == nil else {
+                // Marked.
+                return
+            }
+            
             self.parent.text = textView.text
             selectedTextRange = textView.selectedTextRange
         }
