311f16ffb0647dbbe69f5f2bc706c8bb89ddb6bf
hstdt <hstdt@users.noreply.github.com>
2020-12-04 11:30:44 +0800

    Refactor

--- Sources/HighlightedTextEditor/HighlightedTextEditor.iOS.swift
+++ Sources/HighlightedTextEditor/HighlightedTextEditor.iOS.swift
@@ -95,11 +95,8 @@ public struct HighlightedTextEditor: UIViewRepresentable, HighlightingTextEditor
         
         public func textViewDidChange(_ textView: UITextView) {
 
-            // For Multistage Text Input(Chinese, Japanese)
-            guard  textView.markedTextRange == nil else {
-                // Marked.
-                return
-            }
+            // For Multistage Text Input
+            guard textView.markedTextRange == nil else { return }
             
             self.parent.text = textView.text
             selectedTextRange = textView.selectedTextRange
