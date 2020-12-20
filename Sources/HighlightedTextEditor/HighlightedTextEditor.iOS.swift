06255d9ced95f148634fd250c3beeb262cfb1a83
Christoph Biering <christoph.biering@wandelbots.com>
2020-12-20 13:06:28 +0100

    #19: fixed inseration point color on mac catalyst

--- Sources/HighlightedTextEditor/HighlightedTextEditor.iOS.swift
+++ Sources/HighlightedTextEditor/HighlightedTextEditor.iOS.swift
@@ -83,6 +83,12 @@ public struct HighlightedTextEditor: UIViewRepresentable, HighlightingTextEditor
         let layoutDirection = UIView.userInterfaceLayoutDirection(for: textView.semanticContentAttribute)
         textView.textAlignment = NSTextAlignment(textAlignment: textAlignment, userInterfaceLayoutDirection: layoutDirection)
         textView.tintColor = insertionPointColor ?? textView.tintColor
+    
+        // BUGFIX #19: https://stackoverflow.com/questions/60537039/change-prompt-color-for-uitextfield-on-mac-catalyst
+        #if targetEnvironment(macCatalyst)
+        let textInputTraits = textView.value(forKey: "textInputTraits") as? NSObject
+        textInputTraits?.setValue(textView.tintColor, forKey: "insertionPointColor")
+        #endif
     }
 
     public class Coordinator: NSObject, UITextViewDelegate {
