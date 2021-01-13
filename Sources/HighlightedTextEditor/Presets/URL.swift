c283312c2bfb6631e75f19831ab01214dd1e9689
kyle-n <kylebnazario@gmail.com>
2021-01-13 10:58:42 -0700

    URL preset is now tappable

--- Sources/HighlightedTextEditor/Presets/URL.swift
+++ Sources/HighlightedTextEditor/Presets/URL.swift
@@ -9,16 +9,30 @@ import Foundation
 import SwiftUI
 
 fileprivate let urlRegexPattern = "((?:http|https)://)?(?:www\\.)?[\\w\\d\\-_]+\\.\\w{2,3}(\\.\\w{2})?(/(?<=/)(?:[\\w\\d\\-./_]+)?)?"
-fileprivate let urlRegex = try! NSRegularExpression(pattern: urlRegexPattern, options: [])
+fileprivate let _urlRegex = try! NSRegularExpression(pattern: urlRegexPattern, options: [])
 
 // HighlightedTextEditor.markdown will only be accessible via [HighlightRule].markdown in a future 2.0.0 breaking release
 // It does not really make any sense to have it here
 public extension HighlightedTextEditor {
     static let url: [HighlightRule] = [HighlightRule].url
+    
 }
 
 public extension Sequence where Iterator.Element == HighlightRule {
     static var url: [HighlightRule] {
-        [HighlightRule(pattern: urlRegex, formattingRule: TextFormattingRule(key: .underlineStyle, value: NSUnderlineStyle.single.rawValue))]
+        [
+            HighlightRule(pattern: _urlRegex, formattingRules: [
+                TextFormattingRule(key: .underlineStyle, value: NSUnderlineStyle.single.rawValue),
+                TextFormattingRule(key: .link) { urlString, _ in
+                    URL(string: urlString) as Any
+                }
+            ])
+        ]
+    }
+}
+
+public extension HighlightRule {
+    static var urlRegex: NSRegularExpression {
+        _urlRegex
     }
 }
