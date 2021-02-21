666dd638d5366ac07927b29118e148987c475d2d
ceojosef <79390260+ceojosef@users.noreply.github.com>
2021-02-21 08:15:19 +0200

    Create DetectLineLanguage.swift (RTL support)
    Add supports for multiTL typing

---
+++ Sources/HighlightedTextEditor/DetectLineLanguage.swift
@@ -0,0 +1,24 @@
+final class DetectLineLanguage {
+    let rtlList: Array = ["Hebrew", "Arabic", "Aramaic", "Azeri", "Divehi", "Persian", "Hausa", "Khowar", "Kashmiri", "Kurdish", "Pashto", "Urdu", "Yiddish"]
+    
+    func detect(for string: String) -> Bool {
+        let recognizer = NLLanguageRecognizer()
+        recognizer.processString(string)
+        guard let languageCode = recognizer.dominantLanguage?.rawValue else { return false }
+        let detectedLanguage = Locale.current.localizedString(forIdentifier: languageCode)
+        return self.rtlList.contains(detectedLanguage ?? "English")
+    }
+    
+    func findLink(for string: String) -> Bool {
+        let detector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+        let matches = detector.matches(in: string, options: [], range: NSRange(location: 0, length: string.utf16.count))
+        
+        var count = 0
+        for match in matches {
+            guard let _ = Range(match.range, in: string) else { continue }
+            count += 1
+        }
+        
+        return count > 0
+    }
+}
