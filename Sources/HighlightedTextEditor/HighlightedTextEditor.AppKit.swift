a735119763099ab544881c589d1f0cdf06d02fbb
Kyle Nazario <knazario@bitovi.com>
2023-08-22 11:49:45 -0400

    Runs introspect callback on init editor

--- Sources/HighlightedTextEditor/HighlightedTextEditor.AppKit.swift
+++ Sources/HighlightedTextEditor/HighlightedTextEditor.AppKit.swift
@@ -47,6 +47,7 @@ public struct HighlightedTextEditor: NSViewRepresentable, HighlightingTextEditor
     public func makeNSView(context: Context) -> ScrollableTextView {
         let textView = ScrollableTextView()
         textView.delegate = context.coordinator
+        runIntrospect(textView)
 
         return textView
     }
